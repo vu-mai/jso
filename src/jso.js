b4b9b867260a9c85a4b89b856ff3166b806051e5
Simen F Smaaberg <Simen.F.Smaaberg@visma.com>
2015-04-16 21:16:11 +0200

    Fixed bug

--- src/jso.js
+++ src/jso.js
@@ -401,6 +401,7 @@ define(function(require, exports, module) {
 		var client_id = this.config.get('client_id', null, true);
 		var client_secret = this.config.get('client_secret', null, false);
 		var flow = this.config.get('flow', null, true);
+		var providerID = this.config.get('providerID', null, true);
 
 		utils.log("About to send an authorization request to this entry:", authorization);
 		utils.log("Options", opts, "callback", callback);
@@ -425,7 +426,7 @@ define(function(require, exports, module) {
 
 		request.client_id = client_id;
 
-		request.providerID = this.providerID;
+		request.providerID = providerID;
 
 		var code = store.getCode(providerID);
 
