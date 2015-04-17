6ad0c555b605bdf882749a027b1a0afb48f7b4be
Simen F Smaaberg <Simen.F.Smaaberg@visma.com>
2015-04-17 15:33:27 +0200

    changes

--- src/jso.js
+++ src/jso.js
@@ -508,9 +508,9 @@ define(function(require, exports, module) {
 		JSO.$.ajax(settings).then(function(token){
 			console.log("SUCCESS");
 			console.log(token);
-			token.url = url+"?";
+			token.url = '#' + url;
 
-
+			console.log(token.url);
 			self.callback(utils.encodeURL(token), callback, request.providerID);
 
 		}, function(err){
